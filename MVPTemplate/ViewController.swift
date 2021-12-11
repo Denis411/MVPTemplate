66038faea458a42d4f817ebdc13f6d2b0322f727
Dennis <dnsljnv411@gmail.com>
2021-12-11 21:03:08 +0300

    Initial Commit

---
+++ MVPTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPTemplate
+//
+//  Created by I am the owner on 12/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
