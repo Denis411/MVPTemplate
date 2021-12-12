8413386c29d09ef4683fd6e347b934dd53483171
Dennis <dnsljnv411@gmail.com>
2021-12-12 15:38:27 +0300

    Create background for MainScreen

--- MVPTemplate/MainScreen/Views/MainScreenBackground.swift
+++ MVPTemplate/MainScreen/Views/MainScreenBackground.swift
@@ -1,39 +1,43 @@
 //
-//  ViewController.swift
+//  MainScreenBackground.swift
 //  MVPTemplate
 //
-//  Created by I am the owner on 12/11/21.
+//  Created by I am the owner on 12/12/21.
 //
 
 import UIKit
 
-class ViewController: UIViewController, PresenterMainScreenProtocol {
-    private var presetner = PresenterMainScreen()
-    //    UI
-    var button: UIButton!
-    var textField: UITextField!
-    var label: UILabel!
+final class MainScreenBackground: UIView {
+    private var button: UIButton!
+    private var textField: UITextField!
+    private var label: UILabel!
+    private var closure: (() -> Void)?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        presetner.setUpViewDelegate(delegate: self)
-        view.backgroundColor = .systemGray
+    init() {
+        super.init(frame: .zero)
         setUpButton()
         setUpField()
         setUpLabel()
-        presetner.getText()
     }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpButton()
+        setUpField()
+        setUpLabel()
+    }
+//    MARK: - Constraint methods
     
     private func setUpButton() {
         //      Constraints
         button = UIButton()
-        view.addSubview(button)
+        self.addSubview(button)
         button.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate(
             [ button.heightAnchor.constraint(equalToConstant: 75),
-              button.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
-              button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-              button.centerYAnchor.constraint(equalTo: view.centerYAnchor) ])
+              button.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.8),
+              button.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+              button.centerYAnchor.constraint(equalTo: self.centerYAnchor) ])
         //      Presentation
         button.setTitle("Press to save", for: .normal)
         button.layer.backgroundColor = UIColor.red.cgColor
@@ -44,12 +48,12 @@ class ViewController: UIViewController, PresenterMainScreenProtocol {
     
     private func setUpField() {
         textField = UITextField()
-        view.addSubview(textField)
+        self.addSubview(textField)
         textField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate(
             [ textField.heightAnchor.constraint(equalToConstant: 50),
-              textField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
-              textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+              textField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.8),
+              textField.centerXAnchor.constraint(equalTo: self.centerXAnchor),
               textField.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -50) ])
         textField.layer.backgroundColor = UIColor.yellow.cgColor
         textField.layer.cornerRadius = 20
@@ -65,25 +69,34 @@ class ViewController: UIViewController, PresenterMainScreenProtocol {
     
     private func setUpLabel() {
         label = UILabel()
-        view.addSubview(label)
+        self.addSubview(label)
         label.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate(
             [ label.heightAnchor.constraint(equalToConstant: 50),
-              label.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
-              label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+              label.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.8),
+              label.centerXAnchor.constraint(equalTo: self.centerXAnchor),
               label.bottomAnchor.constraint(equalTo: textField.topAnchor, constant: -50)
             ])
         label.layer.backgroundColor = UIColor.systemGray.cgColor
         label.textAlignment = .center
     }
+// MARK: - Logic methods
     
     @objc private func buttonAction() {
-        guard let text = textField.text else { return }
-        presetner.setText(text: text)
-        presetner.getText()
+        guard let closureNotNil = closure else { return }
+        closureNotNil()
     }
     
-    func updateWord(text: String) {
-        label.text = text
+    internal func setUpButtonAction(closure: @escaping () -> Void) {
+        self.closure = closure
+    }
+    
+    internal func setUpLabelText(text: String) {
+        self.label.text = text
+    }
+    
+    internal func getTextFieldText() -> String {
+        guard let text = textField.text else { return "No text was entered." }
+        return text
     }
 }
